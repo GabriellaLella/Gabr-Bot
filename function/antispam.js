a1d3156e838d7093f3ed6d89b12ad7c7e0c30b27
GabriellaLella <84185597+GabriellaLella@users.noreply.github.com>
2022-02-04 10:21:26 +0100

    Add files via upload

--- function/antispam.js
+++ function/antispam.js
@@ -1,1 +1,22 @@
+// Tempo di recupero in mensajes
+const  usedCommandRecently  =  new  Set ( ) ;
 
+const  isFiltered  =  ( da )  =>  {
+    ritorna  ! ! usedCommandRecently . ha ( da ) ;
+} ;
+
+const  addFilter  =  ( da )  =>  {
+    usedCommandRecently . aggiungi ( da ) ;
+    setTimeout ( ( )  =>  {
+        return  usedCommandRecently . cancella ( da ) ;
+    } ,  3000 ) ; // 3 secondi per il tempo di attesa per il successivo comando;
+} ;
+modulo . esportazioni  =  {
+    msgFilter : {
+        è Filtrato ,
+        aggiungiFiltro
+    } } ;
+© 2022 GitHub, Inc.
+Termini
+Privacy
+Sicure
