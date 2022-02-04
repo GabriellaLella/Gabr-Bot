47c40cc50fc8c7d11384377339a6de43ea1a3004
GabriellaLella <84185597+GabriellaLella@users.noreply.github.com>
2022-02-04 12:24:52 +0100

    Create riddle.js

---
+++ result/game/riddle.js
@@ -0,0 +1,480 @@
+[
+  { "author" : "Gabriella",
+    "index": 1,
+    "soal": "Hewan sejenis kadal yang dapat merubah warna kulitnya?",
+    "jawaban": "Bunglon"
+  },
+  { "author" : "Gabriella",
+    "index": 2,
+    "soal": "Surat instruksi kepada Soeharto untuk mengambil alih keamanan Indonesia pada tanggal 11 Maret 1966?",
+    "jawaban": "Supersemar"
+  },
+  { "author" : "Gabriella",
+    "index": 3,
+    "soal": "Sungai terpanjang di Benua Amerika?",
+    "jawaban": "Amazon"
+  },
+  { "author" : "Gabriella",
+    "index": 4,
+    "soal": "Wanita yang tercatat dalam sejarah sebagai penjahit Bendera Pusaka Indonesia?",
+    "jawaban": "Fatmawati"
+  },
+  { "author" : "Gabriella",
+    "index": 5,
+    "soal": "Lomba lari jarak jauh sepanjang 42,195 km?",
+    "jawaban": "Marathon"
+  },
+  { "author" : "Gabriella",
+    "index": 6,
+    "soal": "Garis yang membagi Bumi menjadi dua bagian belahan utara dan belahan selatan?",
+    "jawaban": "Khatulistiwa"
+  },
+  { "author" : "Gabriella",
+    "index": 7,
+    "soal": "Kota yang berdiri sejak tahun 682 ini disebut sebagai kota tertua di Indonesia?",
+    "jawaban": "Palembang"
+  },
+  { "author" : "Gabriella",
+    "index": 8,
+    "soal": "Sebutan untuk gaya tarik-menarik yang terjadi di alam semesta termasuk Bumi?",
+    "jawaban": "Gravitasi"
+  },
+  { "author" : "Gabriella",
+    "index": 9,
+    "soal": "Nama film yang bercerita tentang robot yang dapat berubah menjadi kendaran?",
+    "jawaban": "Transformers"
+  },
+  { "author" : "Gabriella",
+    "index": 10,
+    "soal": "Petarung di zaman Romawi Kuno yang melakukan pertarungan untuk hiburan umum?",
+    "jawaban": "Gladiator"
+  },
+  { "author" : "Gabriella",
+    "index": 11,
+    "soal": "Nama kapal penumpang mewah yang tenggelam saat pelayaran perdana tahun 1912?",
+    "jawaban": "Titanic"
+  },
+  { "author" : "Gabriella",
+    "index": 12,
+    "soal": "Klub sepak bola dari Italia yang memiliki julukan Nyonya Tua?",
+    "jawaban": "Juventus"
+  },
+  { "author" : "Gabriella",
+    "index": 13,
+    "soal": "Mekanisme sistem pemerintahan negara yang berdasarkan kehendak rakyat?",
+    "jawaban": "Demokrasi"
+  },
+  { "author" : "Gabriella",
+    "index": 14,
+    "soal": "Nama kesenian drama tari tradisional yang khas dari Bali?",
+    "jawaban": "Kecak"
+  },
+  { "author" : "Gabriella",
+    "index": 15,
+    "soal": "Nama mata uang yang digunakan di negara Rusia?",
+    "jawaban": "Rubel"
+  },
+  { "author" : "Gabriella",
+    "index": 16,
+    "soal": "Ilmu alam yang mengamati benda langit serta fenomena di luar atmosfer Bumi?",
+    "jawaban": "Astronomi"
+  },
+  { "author" : "Gabriella",
+    "index": 17,
+    "soal": "Kaisar Prancis yang menguasai Benua Eropa pada tahun 1803-1815?",
+    "jawaban": "Napoleon"
+  },
+  { "author" : "Gabriella",
+    "index": 18,
+    "soal": "Seniman terkenal dunia dari Spanyol dengan aliran seni bernama kubisme?",
+    "jawaban": "Picasso"
+  },
+  { "author" : "Gabriella",
+    "index": 19,
+    "soal": "Nama panglima tertinggi Pasukan Sekutu pada Perang dunia II?",
+    "jawaban": "Eisenhower"
+  },
+  { "author" : "Gabriella",
+    "index": 20,
+    "soal": "Senjata fiksi dalam dunia Star Wars, digunakan oleh Jedi dan Sith?",
+    "jawaban": "Lightsaber"
+  },
+  { "author" : "Gabriella",
+    "index": 21,
+    "soal": "Nama semenanjung di Eropa dimana terdapat negara Norwegia dan Swedia?",
+    "jawaban": "Skandinavia"
+  },
+  { "author" : "Gabriella",
+    "index": 22,
+    "soal": "Stadion sepak bola di London yang juga markas utama klub Arsenal FC?",
+    "jawaban": "Emirates"
+  },
+  { "author" : "Gabriella",
+    "index": 23,
+    "soal": "Penyanyi wanita terkenal dunia, dulu berada dalam grup Destiny’s Child?",
+    "jawaban": "Beyonce"
+  },
+  { "author" : "Gabriella",
+    "index": 24,
+    "soal": "Istilah untuk sebuah kepercayaan bahwa Tuhan adalah satu atau tunggal?",
+    "jawaban": "Monoteisme"
+  },
+  { "author" : "Gabriella",
+    "index": 25,
+    "soal": "Nama kerajaan dimana rajanya menjadi wakil Gubernur DI Yogyakarta saat ini?",
+    "jawaban": "Pakualaman"
+  },
+  { "author" : "Gabriella",
+    "index": 26,
+    "soal": "Disebut sebagai salah satu matematikawan terbesar sejarah, lahir pada 287 SM?",
+    "jawaban": "Archimedes"
+  },
+  { "author" : "Gabriella",
+    "index": 27,
+    "soal": "Rangkaian pegunungan tertinggi di Indonesia, terletak di Pulau Papua?",
+    "jawaban": "Jayawijaya"
+  },
+  { "author" : "Gabriella",
+    "index": 28,
+    "soal": "Kelompok fiksi superhero yang terdiri dari Iron Man, Captain America, Hulk, Thor?",
+    "jawaban": "Avengers"
+  },
+  { "author" : "Gabriella",
+    "index": 29,
+    "soal": "Nama wilayah luas di Rusia, meliputi hampir seluruh wilayah Asia Utara?",
+    "jawaban": "Siberia"
+  },
+  { "author" : "Gabriella",
+    "index": 30,
+    "soal": "Suatu benda atau media yang menghantarkan arus listrik dengan mudah?",
+    "jawaban": "Konduktor"
+  },
+  { "author" : "Gabriella",
+    "index": 31,
+    "soal": "Nama bahasa yang digunakan di Iran, Tajikistan, Afganistan, dan Uzbekistan?",
+    "jawaban": "Farsi"
+  },
+  { "author" : "Gabriella",
+    "index": 32,
+    "soal": "Daerah dimana terdapat praktik pertanian pertama sekitar tahun 8000 SM?",
+    "jawaban": "Mesopotamia"
+  },
+  { "author" : "Gabriella",
+    "index": 33,
+    "soal": "Ideologi yang berdasarkan pada prinsip kepemimpinan dengan otoritas absolut?",
+    "jawaban": "Fasisme"
+  },
+  { "author" : "Gabriella",
+    "index": 34,
+    "soal": "Nama periode geologi dari 252 ke 66 juta tahun yang lalu, disebut juga Zaman Reptil?",
+    "jawaban": "Mesozoikum"
+  },
+  { "author" : "Gabriella",
+    "index": 35,
+    "soal": "Nama yang dikenal dalam legenda Jawab sebagai arsitek perancang Candi Borobudur?",
+    "jawaban": "Gunadharma"
+  },
+  { "author" : "Gabriella",
+    "index": 36,
+    "soal": "Tarian tradisional Tionghoa yang menggunakan kostum menyerupai singa",
+    "jawaban": "Barongsai"
+  },
+  { "author" : "Gabriella",
+    "index": 37,
+    "soal": "Istilah yang artinya perpindahan penduduk dari desa ke kota?",
+    "jawaban": "Urbanisasi"
+  },
+  { "author" : "Gabriella",
+    "index": 38,
+    "soal": "Grup musik terkenal tahun 1970an asal Bandung, personelnya tiga bersaudara?",
+    "jawaban": "Bimbo"
+  },
+  { "author" : "Gabriella",
+    "index": 39,
+    "soal": "Nama film aksi laga Indonesia tahun 2009 yang dibintangi oleh Iko Uwais?",
+    "jawaban": "Merantau"
+  },
+  { "author" : "Gabriella",
+    "index": 40,
+    "soal": "Dikenal sebagai “maestro keroncong Indonesia”, pencipta lagu Bengawan Solo?",
+    "jawaban": "Gesang"
+  },
+  { "author" : "Gabriella",
+    "index": 41,
+    "soal": "Skala suhu dimana titik beku air berada pada 0 derajat dan titik didih pada 100 derajat?",
+    "jawaban": "Celcius"
+  },
+  { "author" : "Gabriella",
+    "index": 42,
+    "soal": "Makhluk halus mitos Jawa, berwujud manusia mirip kera, bertubuh besar dan berbulu?",
+    "jawaban": "Genderuwo"
+  },
+  { "author" : "Gabriella",
+    "index": 43,
+    "soal": "Sebuah distrik di Los Angeles yang terkenal dengan industri perfilmannya yang mendunia?",
+    "jawaban": "Hollywood"
+  },
+  { "author" : "Gabriella",
+    "index": 44,
+    "soal": "Cabang biologi yang mempelajari pewarisan sifat pada organisme maupun suborganisme?",
+    "jawaban": "Genetika"
+  },
+  { "author" : "Gabriella",
+    "index": 45,
+    "soal": "Situs manusia purba di Jawa Tengah, juga merupakan Situs Warisan Dunia UNESCO?",
+    "jawaban": "Sangiran"
+  },
+  { "author" : "Gabriella",
+    "index": 46,
+    "soal": "Bongkahan batu yang berukuran lebih kecil daripada planet, mengorbit Matahari?",
+    "jawaban": "Asteroid"
+  },
+  { "author" : "Gabriella",
+    "index": 47,
+    "soal": "Istilah untuk hewan pemakan rerumputan atau tumbuhan?",
+    "jawaban": "Herbivora"
+  },
+  { "author" : "Gabriella",
+    "index": 48,
+    "soal": "Istilah yang berarti pembantaian sistematis terhadap suatu suku atau kelompok?",
+    "jawaban": "Genosida"
+  },
+  { "author" : "Gabriella",
+    "index": 49,
+    "soal": "Nama penyakit demam yang ditularkan oleh nyamuk Anopheles betina?",
+    "jawaban": "Malaria"
+  },
+  { "author" : "Gabriella",
+    "index": 50,
+    "soal": "Nama tokoh fiksi budaya Sunda yang sifatnya lucu, polos, tetapi sekaligus cerdas?",
+    "jawaban": "Kabayan"
+  },
+  { "author" : "Gabriella",
+    "index": 51,
+    "soal": "Asrama tempat belajar para siswa santri yang dibimbin oleh Kiai?",
+    "jawaban": "Pesantren"
+  },
+  { "author" : "Gabriella",
+    "index": 52,
+    "soal": "Perusahaan video game dari Jepang, pencipta Mario Bros dan Pokemon?",
+    "jawaban": "Nintendo"
+  },
+  { "author" : "Gabriella",
+    "index": 53,
+    "soal": "Sebuah agama resmi di Indonesia yang berdasarkan pada ajaran Konfusius?",
+    "jawaban": "Khong hu cu"
+  },
+  { "author" : "Gabriella",
+    "index": 54,
+    "soal": "Nama ensiklopedia online yang bebas ditulis dan disunting oleh siapa saja?",
+    "jawaban": "Wikipedia"
+  },
+  { "author" : "Gabriella",
+    "index": 55,
+    "soal": "Bangun datar segi empat, sepasang sisi sejajar namun tidak sama panjang?",
+    "jawaban": "Trapesium"
+  },
+  { "author" : "Gabriella",
+    "index": 56,
+    "soal": "Penyakit dimana hormon pengatur gula dara dari tubuh tidak mencukupi?",
+    "jawaban": "Diabetes"
+  },
+  { "author" : "Gabriella",
+    "index": 57,
+    "soal": "Negara independen terkecil di dunia yang diakui secara internasional?",
+    "jawaban": "Vatikan"
+  },
+  { "author" : "Gabriella",
+    "index": 58,
+    "soal": "Kekaisaran kuno di wilayah Yunani yang dipimpin oleh Aleksander Agung?",
+    "jawaban": "Makedonia"
+  },
+  { "author" : "Gabriella",
+    "index": 59,
+    "soal": "Negara dengan jumlah penduduk Muslim terbanyak kedua di dunia?",
+    "jawaban": "Pakistan"
+  },
+  { "author" : "Gabriella",
+    "index": 60,
+    "soal": "Gejala penurunan fungsi otak, umumnya terjadi pada usia lanjut?",
+    "jawaban": "Demensia"
+  },
+  { "author" : "Gabriella",
+    "index": 61,
+    "soal": "Bahan bakar fosil, digunakan di Pembangkit Listrik Tenaga Uap?",
+    "jawaban": "Batu bara"
+  },
+  { "author" : "Gabriella",
+    "index": 62,
+    "soal": "Nama pura terkenal di Bali yang dibangun pada ujung batu karang?",
+    "jawaban": "Uluwatu"
+  },
+  { "author" : "Gabriella",
+    "index": 63,
+    "soal": "Sebuah gelar yang diperoleh putra Sultan Agung dari Mataram?",
+    "jawaban": "Amangkurat"
+  },
+  { "author" : "Gabriella",
+    "index": 64,
+    "soal": "Seorang yang bertugas merawat bayi atau anak pada suatu keluarga?",
+    "jawaban": "Pramusiwi"
+  },
+  { "author" : "Gabriella",
+    "index": 65,
+    "soal": "Daerah di Jepang dimana terjadi kebocoran nuklir pada tahun 2011?",
+    "jawaban": "Fukushima"
+  },
+  { "author" : "Gabriella",
+    "index": 66,
+    "soal": "Perlindungan finansial untuk jiwa kesehatan properti dsb?",
+    "jawaban": "Asuransi"
+  },
+  { "author" : "Gabriella",
+    "index": 67,
+    "soal": "Perusahaan berlogo kuda asal Italia produsen mobil super dan mobil balap?",
+    "jawaban": "Ferrari"
+  },
+  { "author" : "Gabriella",
+    "index": 68,
+    "soal": "Suku dari sulawesi selatan yang terkenal sebagai pelaut ulung?",
+    "jawaban": "BugisVampir yang"
+  },
+  { "author" : "Gabriella",
+    "index": 69,
+    "soal": "Vampir yang merupakan tokoh utama fiksi ciptaan Bram Stoker?",
+    "jawaban": "Drakula"
+  },
+  { "author" : "Gabriella",
+    "index": 70,
+    "soal": "Planet ini merupakan planet terbesar di Tata Surya?",
+    "jawaban": "Yupiter"
+  },
+  { "author" : "Gabriella",
+    "index": 71,
+    "soal": "Tanaman yang dipakai sebagai bahan pembuat kertas pada Zaman Mesir Kuno?",
+    "jawaban": "Papirus"
+  },
+  { "author" : "Gabriella",
+    "index": 72,
+    "soal": "Bahan peledak berupa bubuk campuran dari belerang, arang, dan kalium nitrat?",
+    "jawaban": "Mesiu"
+  },
+  { "author" : "Gabriella",
+    "index": 73,
+    "soal": "Bahasa resmi yang juga digunakan secara luas di Negara Filipina?",
+    "jawaban": "Tagalog"
+  },
+  { "author" : "Gabriella",
+    "index": 74,
+    "soal": "Negara kecil di Amerika Selatan, terdapat banyak keturunan Suku Jawa?",
+    "jawaban": "Suriname"
+  },
+  { "author" : "Gabriella",
+    "index": 75,
+    "soal": "Kuda jantan bersayap dari mitologi Yunani?",
+    "jawaban": "Pegasus"
+  },
+  { "author" : "Gabriella",
+    "index": 76,
+    "soal": "Grup musik Rock alternatif dari London. vokalisnya bernama Chris Martin?",
+    "jawaban": "Coldplay"
+  },
+  { "author" : "Gabriella",
+    "index": 77,
+    "soal": "Buah yang jika dipotong mempunyai penampang berbentuk bintang?",
+    "jawaban": "Belimbing"
+  },
+  { "author" : "Gabriella",
+    "index": 78,
+    "soal": "Ensembel musik yang biasanya menonjolkan metalofon, gambang, gendang, dan gong?",
+    "jawaban": "Gamelan"
+  },
+  { "author" : "Gabriella",
+    "index": 79,
+    "soal": "Nama kota yang selama beberapa abad diperebutkan oleh Islam, Kristen, Yahudi?",
+    "jawaban": "Yerusalem"
+  },
+  { "author" : "Gabriella",
+    "index": 80,
+    "soal": "Bahasa resmi negara Bangladesh, digunakan oleh sekitar 200 juta orang?",
+    "jawaban": "Bengali"
+  },
+  { "author" : "Gabriella",
+    "index": 81,
+    "soal": "Minuman memabukkan yang berbahan dasar etanol?",
+    "jawaban": "Alkohol"
+  },
+  { "author" : "Gabriella",
+    "index": 82,
+    "soal": "Satu-satunya gunung berapi aktif di Eropa Daratan yang letaknya di Italia?",
+    "jawaban": "Vesuvius"
+  },
+  { "author" : "Gabriella",
+    "index": 83,
+    "soal": "Karakter fiksi berambut sangat panjang tokoh utama film animasi Tangled?",
+    "jawaban": "Rapunzel"
+  },
+  { "author" : "Gabriella",
+    "index": 84,
+    "soal": "Bencana alam berupa gelombang ombak besar dari laut?",
+    "jawaban": "Tsunami"
+  },
+  { "author" : "Gabriella",
+    "index": 85,
+    "soal": "Nama kota di negara Turki yang zaman dulu bernama Konstantinopel?",
+    "jawaban": "Istanbul"
+  },
+  { "author" : "Gabriella",
+    "index": 86,
+    "soal": "Subspesies manusia yang punah dari muka bumi sekitar 30.000 tahun yang lalu?",
+    "jawaban": "Neanderthal"
+  },
+  { "author" : "Gabriella",
+    "index": 87,
+    "soal": "Nama ritual bunuh diri kaum Samurai di Jepang dengan cara merobek perut?",
+    "jawaban": "Seppuku"
+  },
+  { "author" : "Gabriella",
+    "index": 88,
+    "soal": "Jenis keju asal Italia yang seringkali terdapat pada hidangan pizza?",
+    "jawaban": "Mozzarella"
+  },
+  { "author" : "Gabriella",
+    "index": 89,
+    "soal": "Gelar bagi istri dari penguasa monarki pria(raja, sultan, atau kaisar)?",
+    "jawaban": "Permaisuri"
+  },
+  { "author" : "Gabriella",
+    "index": 90,
+    "soal": "Nama suku yang sejak lama mendiami daerah kutub utara bumi?",
+    "jawaban": "Eskimo"
+  },
+  { "author" : "Gabriella",
+    "index": 91,
+    "soal": "Unsur paling berlimpah kedua setelah oksigen pada tubuh manusia?",
+    "jawaban": "Karbon"
+  },
+  { "author" : "Gabriella",
+    "index": 92,
+    "soal": "Peristiwa genosida terhadap penganut Yahudi Eropa selama Perang Dunia II?",
+    "jawaban": "Holokaus"
+  },
+  
+  { "author" : "Gabriella",
+    "index": 146,
+    "soal": "Rempah dari biji keluarga jahe-jahean, merupakan rempah termahal ketiga di dunia?",
+    "jawaban": "Kapulaga"
+  },
+  { "author" : "Gabriella",
+    "index": 147,
+    "soal": "Benua yang meliputi Kutub Selatan bumi memiliki suhu yang sangat rendah?",
+    "jawaban": "Antartika"
+  },
+  { "author" : "Gabriella",
+    "index": 148,
+    "soal": "Senjata pelontar yang banyak digunakan pada Abad Pertengahan untuk menghancurkan dinding?",
+    "jawaban": "Trebuset"
+  },
+
+  }
+]
