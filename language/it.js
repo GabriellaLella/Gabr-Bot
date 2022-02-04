7ae265bd4dc804d3ac0edf7f9dc3afe5751bb459
GabriellaLella <84185597+GabriellaLella@users.noreply.github.com>
2022-02-04 11:54:22 +0100

    Add files via upload

---
+++ language/it.js
@@ -0,0 +1,267 @@
+// comando vocale
+esportazioni . vnCmd  =  ( v )  =>  {
+  ritorna  'Utilizza i comandi con i promemoria vocali, attiva con il comando '  +  v  +  'voicecommand on/off' ;
+} ;
+
+// -- invia messaggio 
+esportazioni . wait  =  `*⌛ _Caricamento in corso..._ ▬▬▬▭*` ;
+esportazioni . ciao  =  `Arrivederci...` ;
+esportazioni . fatto  =  `✅ Finito...` ;
+esportazioni . successivo  =  ( valore )  =>  {
+  return  `Fai clic su Avanti per * ${ valore } *` ;
+} ;
+esportazioni . packon  =  'Nome pacchetto già registrato' ;
+esportazioni . packoff  =  'Il nome del media pack non è registrato nel database' ;
+esportazioni . liston  =  '✅ Elenco dei media archiviati nel database' ;
+esportazioni . getlist  =  '✳️ Usa il comando getimg/getvid/getav/getstik per recuperare ogni media pack\n*📌 Esempio* : /getimg auron' ;
+esportazioni . nolink  =  ( valore )  =>  {
+  return  '✳️ Ripeti aggiungendo un link'  +  valore ;
+} ;
+//--nuovo
+esportazioni . lvl  =  `Livello` ;
+esportazioni . nme  =  `Nome` ;
+esportazioni . ciao  =  `Ciao` ;
+esportazioni . gp  =  `Gruppo` ;
+esportazioni . restart  =  `✅ Riavvio del bot` ;
+esportazioni . pinsp  =  `Velocità` ;
+esportazioni . pinse  =  `Secondi` ;
+esportazioni . num  =  `Numero` ;
+esportazioni . click  =  `Clicca qui` ;
+esportazioni . next2  =  `Avanti` ;
+esportazioni . stima  =  `✳️ Invia un'immagine con il comando\n o tagga un'immagine che è stata inviata, *Video 1-9 secondi* ` ;
+esportazioni . risultato  =  `Risultato` ; 
+esportazioni . bugr  =  `Grazie \nLa tua segnalazione è stata archiviata nel database.` ;
+esportazioni . linkgp  =  `Collegamento di gruppo` ;
+esportazioni . linkrevo  =  `Collegamento gruppo annullato` ;
+esportazioni . newlink  =  `Nuovo collegamento` ;
+esportazioni . gpbt  =  `🛡️ Impostazioni gruppo\nApri e chiudi il gruppo` ;
+esportazioni . gpbtt  =  `Scegli uno sotto` ;
+esportazioni . gpopenb  =  `APERTO` ;
+esportazioni . gpcloseb  =  `CHIUDI` ;
+esportazioni . idioma  =  `lingua` ;
+esportazioni . wrn  =  `Avviso` ;
+esportazioni . pfile  =  `PROFILO` ;
+esportazioni . crtio  =  `Il` ;
+esportazioni . mbr  =  `Membri` ;
+esportazioni . wlme  =  `Benvenuto` ;
+esportazioni . vtc  =  `VOTING` ;
+esportazioni . rzon  =  `Motivo` ;
+esportazioni . vyes  =  `Se sei d'accordo` ;
+esportazioni . vno  =  `Se non sei d'accordo` ;
+esportazioni . vrvt  =  `Vedi Votazioni` ;
+esportazioni . rtvt  =  `RISULTATI DELLA VOTAZIONE` ;
+esportazioni . dlte  =  `Elimina` ;
+esportazioni . setpmulti  =  `✅ Il prefisso è stato modificato in : *multiprefix*` ;
+esportazioni . setpnopre  =  `✅ Il prefisso è stato modificato in : *nopref*` ;
+esportazioni . setponepre  =  `✅ Il prefisso è stato modificato in :` ;
+esportazioni . setpall  =  `Prefissi disponibili` ;
+esportazioni . setpnp  =  `Nessun prefisso` ;
+esportazioni . setpop  =  `Un prefisso` ;
+esportazioni . setpmt  =  `Multiprefisso` ;
+esportazioni . updatef  =  `*Aggiornato*\n\nmodifiche` ;
+esportazioni . simn  =  `vuoi chattare per un po'?\n\nRispondi con` ;
+esportazioni . simmsg  =  `(Il tuo messaggio)\n\n📌 Esempio : ` ;
+esportazioni . chatboton  =  `✅ Hai attivato *ChatBot* \n Ciò significa che possiamo chattare illimitatamente 😊` ;
+esportazioni . chatbotoff  =  `✅ *ChatBot* disabilitato` ; 
+esportazioni . pptt  =  `Seleziona per favore` ;
+esportazioni . esempio = ` Esempio`  ; 
+esportazioni . emt  =  `Lega` ;
+esportazioni . gst  =  `Vinto` ;
+esportazioni . pdt  =  `Hai perso` ;
+esportazioni . pptuser  =  `Tu` ;
+esportazioni . carta  =  `carta` ;
+esportazioni . tijera  =  `forbice` ;
+esportazioni . piedra  =  `pietra` ;
+esportazioni . foll  =  `Seguimi su Instagram \nhttps://www.instagram.com/fg98._ ` ;
+esportazioni . plays  =  `Inserisci il titolo di un brano` ;
+esportazioni . playm  =  `Il file è troppo grande, la riproduzione della musica è stata annullata, la dimensione massima del file è` ;
+esportazioni . listgp  =  `LISTA dei gruppi` ;
+esportazioni . calidad  =  `Qualità` ;
+esportazioni . tamaño  =  `Taglia` ;
+esportazioni . pfo  =  `Scegli un formato\n\nSe hai problemi con il comando use` ;
+esportazioni . pafo  =  `Seleziona la musica che vuoi scaricare` ;
+esportazioni . pvfo  =  `Seleziona il video che vuoi scaricare` ;
+esportazioni . titlelp  =  `Titolo` ;
+esportazioni . timp  =  `Durata` ;
+esportazioni . viep  =  `Viste` ;
+esportazioni . vcnrst  =  'Non rilevato' ;
+esportazioni . vclect  =  'Lettura vocale' ;
+esportazioni . listwb  =  'Elenco eventi' ;
+esportazioni . pregt  =  'Lei mi ama?' ;
+esportazioni . donare  =  `≡ *DONAZIONE*
+puoi donare se vuoi aiutare a mantenere attivo il bot
+▢ *PayPal*
+• *Link :* https://tinyurl.com/PayPal-fg
+_Donando ottieni_ *Premium* ` ;
+esportazioni . cretb  =  'Crea il tuo bot usando Termux' ;
+
+// -- Dire l'ora
+esportazioni . notte  =  'Buonanotte'
+esportazioni . sera  =  'Buon pomeriggio'
+esportazioni . giorno  =  'Buona giornata'
+esportazioni . mattina  =  'Buongiorno'
+
+
+// -- solo messaggio
+esportazioni . admin  =  '✳️ Spiacenti, questo comando può essere eseguito solo da amministratori di gruppo' ;
+esportazioni . group  =  '✳️ Siamo spiacenti, questo comando può essere eseguito solo in gruppi' ;
+esportazioni . premium  =  '✳️ Spiacenti, questo comando può essere utilizzato solo da utenti premium' ;
+esportazioni . premdl  =  '✳️ Siamo spiacenti di non essere un utente premium, scaricalo tu stesso utilizzando il link\n*LINK* : ' ;
+esportazioni . botadmin  =  '✳️ Questo comando può essere utilizzato quando il bot diventa admin' ;
+esportazioni . proprietario  =  '✳️ Questo comando può essere utilizzato dai robot proprietari' ;
+esportazioni . isprem  =  '✳️ L'utente è l'utente premium precedente' ;
+esportazioni . noprem  =  '✳️ L'utente non è ancora un utente premium' ;
+esportazioni . ban  =  '✳️ Lo stato dell'utente è stato bannato in precedenza' ;
+esportazioni . noban  =  '✳️ L'utente non ha lo stato bannato' ;
+esportazioni . isadmin  =  '✳️ Il bot non può accedere all'amministratore' ;
+esportazioni . oversize  =  '⚠️ La dimensione del file supera la dimensione specificata, scaricalo tu stesso tramite il link sottostante\n*🔗 Link* : ' ;
+
+// -- testo
+esportazioni . notag  =  '✳️ Tagga un membro del gruppo' ;
+esportazioni . nonum  =  '✳️ Ripeti aggiungendo il numero di destinazione' ;
+esportazioni . nottext  =  '✳️ Ripeti aggiungendo del testo' ;
+esportazioni . answer = ' ✳️  Rispondi al messaggio di destinazione...' ; 
+esportazioni . answerStic  =  '✳️ Adesivo messaggio di risposta...' ;
+esportazioni . answerVid  =  '✳️ Rispondi al video...' ;
+esportazioni . answerVn  =  '✳️ Rispondi all'audio...' ;
+esportazioni . answerImg  =  '✳️ Rispondi all'imagenya...' ;
+esportazioni . noreply  =  '✳️ Il messaggio a cui hai risposto non conteneva una risposta' ;
+esportazioni . nolink  =  ( valore )  =>  {
+  return  `✳️ Ripeti aggiungendo un link * ${ value } *` ;
+} ;
+esportazioni . addwarn  =  `⚠️ Avviso\nRicevi 1 avviso` ;
+esportazioni . delwarn  =  `⚠️ Avviso\nIl tuo avviso è diminuito di 1 avviso` ;
+esportazioni . cekwarn  =  ( avviso )  =>  {
+  return  `≡ AVVISI \n\n▢ Totale : * ${ warning } *` ;
+} ;
+esportazioni . nowarn  =  `✳️ L'utente non ha alcun avviso` ;
+ 
+esportazioni . Pbahasa  =  `✳️ Seleziona la lingua che desideri utilizzare
+*Lingue disponibili*
+- es (spagnolo)
+- id (Indonesia)
+- it (inglese)
+- pt (Portoghese)
+📌 Esempio : */lingua es*` ;
+esportazioni . nobahasa  =  `Lingua non disponibile
+*Lingue disponibili*
+- es (spagnolo)
+- id (Indonesia)
+- it (inglese)
+- pt (Portoghese)
+📌 Esempio : */lingua es*` ;
+
+esportazioni . online  =  '✅ Attivazione riuscita del bot in questo gruppo' ;
+esportazioni . offline  =  '✅ Disattivato con successo i bot in questo gruppo' ;
+
+// -- gruppo
+esportazioni . onwa  =  '✳️ L'utente è già nel gruppo' ;
+esportazioni . sendlink  =  '✅ Invia un invito' ;
+esportazioni . open  =  '✅ Impostazioni del gruppo modificate per consentire a tutti i membri di inviare messaggi in questo gruppo' ;
+esportazioni . close  =  '✅ Impostazioni del gruppo modificate per consentire solo agli amministratori di inviare messaggi in questo gruppo' ;
+esportazioni . nome  =  ( valore )  =>  {
+  return  `✅ Cambia l'oggetto in * ${ valore } *` ;
+} ;
+esportazioni . scrivania  =  ( valore )  =>  {
+  return  `✅ Modificata la descrizione del gruppo in \n\n ${ valore } ` ;
+} ;
+esportazioni . promuovi  =  ( valore )  =>  {
+  return  `✅ Promosso con successo * ${ valore } * come amministratore` ;
+} ;
+esportazioni . retrocedere  =  ( valore )  =>  {
+  ritorno  '✅ Retrocesso con successo'  +  valore ;
+} ;
+
+esportazioni . kick  =  ( valore )  =>  {
+  reso ' ✅Ordine  ricevuto, emesso' + valore ;
+} ;
+esportazioni . On  =  ( valore )  =>  {
+ return  `✅ Attiva * ${ valore } * in questo gruppo` ;
+} ;
+esportazioni . Spento  =  ( valore )  =>  {
+  return  `✅ * ${ valore } * disattivato per questo gruppo` ;
+} ;
+esportazioni . Thison  =  ( valore )  =>  {
+  return  `✳️ * ${ valore } * è stato attivato prima` ;
+} ;
+esportazioni . Thisoff  =  ( valore )  =>  {
+return  `✳️ * ${ valore } * non attivato prima` ;
+} ;
+esportazioni . OnorOff  =  '✳️ Ripeti aggiungendo on/off' ;
+esportazioni . antilink  =  '❎ Non permettiamo link da altri gruppi!\nSpiacenti, verrai espulso' ;
+
+esportazioni . setwel  =  ( valore )  =>  { 
+  return  `✳️ Inserisci il tuo messaggio di *benvenuto*
+*📌 Esempio:*
+/setwelcome Benvenuto @utente in @gruppo
+ 
+Etichetta: @utente
+Nome : @nome
+Biografia: @bio
+Data : @data
+Nome gruppo : @gruppo
+Descrizione: @desc
+*Esempi per ogni funzione*\n`  +  valore ;
+} ;
+
+esportazioni . addio  =  ( valore )  =>  {
+  return  `✳️ Inserisci il tuo messaggio di *Addio*
+  
+*📌 Esempio:*
+/addio @utente
+*Esempi per ogni funzione*\n`  +  valore ;
+} ;
+
+esportazioni . setweldone  =  ( valore ,  funghi )  =>  {
+  restituisci  `✅ set di messaggi di *Benvenuto*` ;
+} ;
+
+esportazioni . setbyedone  =  ( valore ,  funghi )  =>  {
+  restituisci  `✅ *ciao* set di messaggi` ;
+} ;
+//--
+esportazioni . predefinito  =  ( valore )  =>  {
+   valore di  ritorno +  'Ritorna alle impostazioni iniziali' ;
+} ;
+
+esportazioni . principale  =  ( valore )  =>  {
+  return  '✳️ C'è ancora '  +  valore  +  ' in corso!' ;
+} ;
+esportazioni . nome principale  =  ( valore )  =>  {
+  return  '✳️ Non c'è nessun '  +  valore  +  ' in corso!' ;
+} ;
+esportazioni . inmain  =  ( valore )  =>  {
+  restituisce  'Hai fatto'  +  valore
+} ;
+esportazioni . hapus  =  ( valore )  => {
+  return  `✅ Eliminato con successo * ${ valore } * in questo gruppo` ;
+} ;
+
+// gioco
+esportazioni . onGame  =  '⚠️ Ci sono ancora domande senza risposta in questa chat' ;
+esportazioni . soal  =  ( testo1 ,  testo2 , testo3 ) => {   
+  ritorno  ` ${ testo1 }
+*⏳ Timeout*
+${ testo2 }
+*PUNTO*
+${ testo3 }
+Rispondi a questo messaggio per rispondere
+Il suggerimento per la risposta appare negli ultimi 10 secondi` ;
+} ;
+
+esportazioni . timeout  =  '⏳ Timeout la risposta è ' ;
+esportazioni . salah  =  '*❎ Sbagliato* !\nRiprova' ;
+esportazioni . hampir  =  '✳️ *Ancora un po'* !\nRiprova' ;
+esportazioni . benar  =  ( valore ,  valore2 )  =>  {
+  return  `✅ *Destra*\nLa risposta è ${ valore } \nottieni ${ valore2 } Monete` ;
+} ;
+
+// afk
+esportazioni . con  =  'con ragione' ;
+esportazioni . onAfk  =  ( valore )  =>  {
+  restituisce  'ora sei afk'  +  valore
+} ;
+esportazioni . offAfk  =  'Sei tornato da AFK'
+esportazioni . inAfk  =  ( valore ,  ora )  =>  {
+  return  'L'utente è in modalità Apk '  +  valore  +  '\n On : '  +  ora
+}
