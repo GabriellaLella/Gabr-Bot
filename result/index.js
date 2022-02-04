722f692bbd4e3ddc9b55c6418dc2b428c0589991
GabriellaLella <84185597+GabriellaLella@users.noreply.github.com>
2022-02-04 22:06:15 +0100

    Update index.js

--- result/index.js
+++ result/index.js
@@ -9,42 +9,18 @@ function pickRandom(list) {
 }
 
 // -- file from result json
-const _truth = readfile('./result/random/truth.json');
-const _dare = readfile('./result/random/dare.json');
-const _dilan = readfile('./result/random/dilan.json');
-const _fakta = readfile('./result/random/fakta.json');
-const _gombal = readfile('./result/random/gombal.json');
-const _hacker = readfile('./result/random/hacker.json');
-const _ilham = readfile('./result/random/hacker.json');
+const _verdad = readfile('./result/random/verdad.json');
+const _reto = readfile('./result/random/reto.json');
 
 // -- get results
-const truth = () => {
- return pickRandom(_truth);
+const verdad = () => {
+ return pickRandom(_verdad);
 };
 
-const dare = () => {
- return pickRandom(_dare);
+const reto = () => {
+ return pickRandom(_reto);
 };
 
-const dilan = () => {
- return pickRandom(_dilan);
-};
-
-const ilham = () => {
- return pickRandom(_ilham);
-};
-
-const fakta = () => {
- return pickRandom(_fakta);
-};
-
-const gombal = () => {
- return pickRandom(_gombal);
-};
-
-const hacker = () => {
- return pickRandom(_hacker);
-};
 
 
 // -- result download
@@ -52,24 +28,16 @@ const { pinterest } = require('./download/pinterest_img');
 const { igDl } = require('./download/instagramPost_dl');
 const { Ttdl } = require('./download/tiktokPost_dl');
 const { yta, ytv } = require('./download/yt_download');
+const { mfire } = require('./download/mediafire.js')
 // -- result search
 const { playstore } = require('./search/playstore');
-const { RandomCerpen } = require('./search/cerpen');
 
 // information
-const { covid  } = require('./informasi/corona');
 const { namaninja, purba, blackpill, typewriter, sans, castle } =  require('./informasi/text_generator');
-const { ArtiMimpi, ArtiNama, zodiakHari, zodiakMinggu } = require('./informasi/primbon');
 
 module.exports = {
-  truth,
-  dare,
-  dilan,
-  ilham,
-  fakta,
-  gombal,
-  hacker,
-  covid,
+  verdad,
+  reto,
   namaninja,
   purba,
   blackpill, 
@@ -78,13 +46,9 @@ module.exports = {
   castle,
   pinterest,
   playstore,
-  RandomCerpen,
   igDl,
   Ttdl,
   yta,
-  ytv,
-  ArtiMimpi,
-  ArtiNama,
-  zodiakHari,
-  zodiakMinggu
+  ytv, 
+  mfire
 };
